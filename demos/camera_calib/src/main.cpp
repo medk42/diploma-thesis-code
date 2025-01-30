ae769b97122bcf4c6ce6af5a541e3a070ab267ed
Jakub Medek <medekjak@gmail.com>
2025-01-30 19:11:22 +0100

    Create project structure.

---
+++ demos/camera_calib/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// #include <opencv2/core.hpp>
+// #include <opencv2/aruco.hpp>
+// #include <opencv2/imgcodecs.hpp>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello world!" << std::endl;
+
+    // cv::Mat markerImage;
+    // cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    // cv::aruco::generateImageMarker(dictionary, 23, 200, markerImage, 1);
+    // cv::imwrite("marker23.png", markerImage);
+
+    return 0;
+}
