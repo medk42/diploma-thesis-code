9d1cf74a6307d9bc7a0c81901850b424968a2417
Jakub Medek <medekjak@gmail.com>
2025-01-30 19:09:49 +0100

    Update pen demo for the current pen firmware.

--- parts/pen/pen_code/python/ble_demo.py
+++ parts/pen/pen_code/python/ble_demo.py
@@ -4,16 +4,24 @@ import struct
 import time
 
 PEN_SERVICE_ID = '2bfae565-df4e-45b6-b1fa-a6f75c1be2b3'
-
 PEN_CHARACTERISTIC_UUID = 'e76d106d-a549-4b3a-afbd-8879582943fe'
+
+FLAG_VALID = 1
+FLAG_BUT_PRIM_PRESSED = 2
+FLAG_BUT_SEC_PRESSED = 4
 
 
 import struct
 
 last_notify = time.time()
+but_prim_last = False
+but_sec_last = False
 
 def notification_handler(sender, data):
     global last_notify
+    global but_prim_last
+    global but_sec_last
+
     current_time = time.time()
     time_diff = current_time - last_notify
     last_notify = current_time
@@ -22,7 +30,7 @@ def notification_handler(sender, data):
     # '<' for little-endian, '3h' for 3 int16_t, '3h' for 3 int16_t, 'H' for uint16_t
     format_string = "<3h3hH"
 
-    print(len(data), data)
+    # print(len(data), data)
     # Unpack the data
     unpacked_data = struct.unpack(format_string, data)
 
@@ -31,8 +39,29 @@ def notification_handler(sender, data):
     gyro = unpacked_data[3:6]
     flags = unpacked_data[6]
 
+    # Check that packet is valid
+    if not (flags & FLAG_VALID):
+        return
+    
+    but_prim_pressed = True if flags & FLAG_BUT_PRIM_PRESSED else False
+    but_sec_pressed = True if flags & FLAG_BUT_SEC_PRESSED else False
+
+    if but_prim_pressed and not but_prim_last:
+        print('Primary button: PRESSED')
+    if not but_prim_pressed and but_prim_last:
+        print('Primary button: RELEASED')
+    if but_sec_pressed and not but_sec_last:
+        print('Secondary button: PRESSED')
+    if not but_sec_pressed and but_sec_last:
+        print('Secondary button: RELEASED')
+
+    but_prim_last = but_prim_pressed
+    but_sec_last = but_sec_pressed
+
     # Print the data
-    print(f"Time diff: {time_diff * 1000:.1f}ms, Accelerometer: {accel}, Gyroscope: {gyro}, Flags: {flags}")
+    # print(f"Time diff: {time_diff * 1000:.1f}ms, Accelerometer: {accel}, Gyroscope: {gyro}, Flags: {flags}")
+
+        
 
 async def communicate(client):
     if client.is_connected:
@@ -44,7 +73,7 @@ async def communicate(client):
             for characteristic in service.characteristics:
                 print(f"  Characteristic: {characteristic.uuid} ({characteristic.properties})")
         await client.start_notify(PEN_CHARACTERISTIC_UUID, notification_handler)
-        await asyncio.sleep(1)
+        await asyncio.sleep(10)
         await client.stop_notify(PEN_CHARACTERISTIC_UUID)
         print("Disconnected")
     else:
@@ -53,6 +82,9 @@ async def communicate(client):
 async def main():
     # Create a BleakScanner
     scanner = bleak.BleakScanner()
+
+    device = await bleak.BleakScanner.find_device_by_name("3D Pen", timeout=5)
+    print(device)
 
     # Start scanning for devices
     print("Scanning for devices...")
