1cef33fc5d2dde2a4fd40903f6440b0c00980a5b
Jakub Medek <medekjak@gmail.com>
2025-03-26 13:38:08 +0100

    Reading data demo.

--- demos/pen_tracking/src/main_ble_test.cpp
+++ demos/pen_tracking/src/main_ble_test.cpp
@@ -6,7 +6,22 @@
 #include <thread>
 #include <algorithm>
 #include <mutex>
+#include <chrono>
 
+int64_t millis()
+{
+    auto now = std::chrono::system_clock::now();
+    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+    auto value = now_ms.time_since_epoch();
+    return value.count();
+}
+
+
+struct IMUDataPacket {
+   int16_t accel[3];
+   int16_t gyro[3];
+   uint16_t flags; // 1 = valid, 2 = button primary, 4 = button secondary
+ };
 
 
 int main(int argc, char** argv) {
@@ -28,62 +43,103 @@ int main(int argc, char** argv) {
 
 
    const std::string SERVICE_UUID = "2bfae565-df4e-45b6-b1fa-a6f75c1be2b3";
+   const std::string CHARACTERISTIC_UUID = "e76d106d-a549-4b3a-afbd-8879582943fe";
+   const int SCAN_TIME_MS = 10000;
 
 
-   // std::optional<SimpleBLE::Peripheral> peripheral;
-   // bool found;
-   // std::mutex peripheral_mutex;
+   SimpleBLE::Peripheral peripheral;
+   bool found = false;
+   std::mutex peripheral_mutex;
+   bool stopped = false;
 
-   adapter.set_callback_on_scan_found([SERVICE_UUID](SimpleBLE::Peripheral peripheral) {
-      std::string identifier = peripheral.identifier();
-
-      std::vector<SimpleBLE::Service> services = peripheral.services();
-      // std::find_if(services.begin(), services.end(), [SERVICE_UUID](SimpleBLE::Service& service){
-      //    return service.uuid() == SERVICE_UUID
-      // })
-      for (SimpleBLE::Service& service : services)
+   adapter.set_callback_on_scan_found([SERVICE_UUID, &found, &peripheral, &peripheral_mutex](SimpleBLE::Peripheral p) {
+      std::vector<SimpleBLE::Service> services = p.services();
+      auto it = std::find_if(services.begin(), services.end(), [SERVICE_UUID](SimpleBLE::Service& service){
+         return service.uuid() == SERVICE_UUID;
+      });
+      
+      if (it != services.end())
       {
-         if (service.uuid() == SERVICE_UUID)
-         {
-            AERGO_LOG("FOUND PEN!")
-            std::cout << "Peripheral found: " << (identifier.empty() ? "<NO_NAME>" : identifier) << " (" << peripheral.address() << ")" << std::endl;
-         }
+         std::lock_guard<std::mutex> lock(peripheral_mutex);
+         peripheral = p;
+         found = true;
       }
    });
 
-   adapter.set_callback_on_scan_updated([](SimpleBLE::Peripheral peripheral) {
-      std::string identifier = peripheral.identifier();
-      if (peripheral.address() == "c9:4d:c0:e9:04:c5")
-      {
-         for (auto service : peripheral.services())
-      {
-         std::cout << "Peripheral UPDATED: " << (identifier.empty() ? "<NO_NAME>" : identifier) << " (" << peripheral.address() << ")" << std::endl;
-
-         AERGO_LOG("\tSERVICE " << service.uuid())
-         for (auto ch : service.characteristics())
-         {
-            AERGO_LOG("\t\tCHARACTERISTIC " << ch.uuid())
-            for (auto des : ch.descriptors())
-            {
-               AERGO_LOG("\t\t\tDESCRIPTOR " << des.uuid())
-            }
-            if (ch.descriptors().size() == 0)
-            {
-               AERGO_LOG("\t\t\tNO DESCRIPTOR")
-            }
-         }
-         if (service.characteristics().size() == 0)
-         {
-            AERGO_LOG("\t\tNO CHARACTERISTIC")
-         }
-      }
-      }
-         
+   adapter.set_callback_on_scan_stop([&stopped, &peripheral_mutex](){
+      std::lock_guard<std::mutex> lock(peripheral_mutex);
+      stopped = true;
    });
+
 
    adapter.scan_start();
-   std::this_thread::sleep_for(std::chrono::seconds(5));
+
+   bool success = false;
+   int64_t start_time = millis();
+   while (millis() < start_time + SCAN_TIME_MS)
+   {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      {
+         std::lock_guard<std::mutex> lock(peripheral_mutex);
+         if (found)
+         {
+            success = true;
+            break;
+         }
+      }
+   }
+
    adapter.scan_stop();
+   while (true)
+   {
+      {
+         std::lock_guard<std::mutex> lock(peripheral_mutex);
+         if (stopped)
+         {
+            break;
+         }
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   }
+
+   if (!success)
+   {
+      AERGO_LOG("Scan failed to find the \"3D Pen\" device.")
+      return 1;
+   }
+
+   AERGO_LOG("Successfully got the \"3D Pen\" device, connecting...")
+
+   peripheral.connect();
+
+   if (!peripheral.is_connected())
+   {
+      AERGO_LOG("Failed to connect");
+      return 1;
+   }
+
+   AERGO_LOG("Connected!")
+
+   int64_t last_time_ms = millis();
+   peripheral.notify(SERVICE_UUID, CHARACTERISTIC_UUID, [&last_time_ms](SimpleBLE::ByteArray payload) {
+      if (payload.size() != sizeof(IMUDataPacket))
+      {
+         AERGO_LOG("WARN: expected size " << sizeof(IMUDataPacket) << ", actual size " << payload.size())
+      }
+      IMUDataPacket imu_data_packet;
+      std::memcpy(&imu_data_packet, payload.data(), payload.size());
+
+      int64_t curr_ms = millis();
+      double freq = 1000.0 / (curr_ms - last_time_ms);
+      last_time_ms = curr_ms;
+
+      // AERGO_LOG("IMU Data: Accel: " << imu_data_packet.accel[0] << ", " << imu_data_packet.accel[1] << ", " << imu_data_packet.accel[2] 
+      //    << "\nGyro: " << imu_data_packet.gyro[0] << ", " << imu_data_packet.gyro[1] << ", " << imu_data_packet.gyro[2] 
+      //    << "\nFlags: " << imu_data_packet.flags)
+      AERGO_LOG("Frequency: " << freq << "Hz\n")
+   });
+
+   std::this_thread::sleep_for(std::chrono::seconds(10));
 
    return 0;
 }
