42d3ffe707c33022b0119582064d8e98616202bc
Jakub Medek <medekjak@gmail.com>
2025-07-14 14:29:12 +0200

    Implement shared memory wrapper in core.

---
+++ demos/modules/core/include/core/memory_allocation/shared_data_core.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "module_common/module_interface_threads.h"
+
+namespace aergo::core::memory_allocation
+{
+    class SharedDataCore : public aergo::module::ISharedData
+    {
+    public:
+        SharedDataCore() noexcept;
+        ~SharedDataCore() noexcept override;
+
+        SharedDataCore(const SharedDataCore& other) = delete;
+        SharedDataCore(SharedDataCore&& other) noexcept;
+
+        SharedDataCore& operator=(const SharedDataCore& other) = delete;
+        SharedDataCore& operator=(SharedDataCore&& other) noexcept;
+
+        virtual bool valid() override final;
+        virtual uint8_t* data() override final;
+        virtual uint64_t size() override final;
+        uint64_t id();
+
+        uint64_t counter();
+        void increaseCounter();
+
+        /// @brief Does not decrease below zero. 
+        void decreaseCounter();
+
+        static SharedDataCore allocate(uint64_t size, uint64_t id) noexcept;
+
+    private:
+        SharedDataCore(uint8_t* data, uint64_t size, uint64_t id);
+    
+        uint8_t* data_;
+        uint64_t size_;
+        uint64_t id_;
+        bool valid_;
+        uint64_t counter_;
+    };
+}
---
+++ demos/modules/core/src/memory_allocation/shared_data_core.cpp
@@ -0,0 +1,129 @@
+#include "core/memory_allocation/shared_data_core.h"
+
+#include <cstdlib>
+
+using namespace aergo::core::memory_allocation;
+
+
+
+SharedDataCore::SharedDataCore() noexcept
+: data_(nullptr), size_(0), id_(0), valid_(false), counter_(0) {}
+
+
+
+SharedDataCore::SharedDataCore(uint8_t* data, uint64_t size, uint64_t id) 
+: data_(data), size_(size), id_(id), valid_(true), counter_(0) {}
+
+
+
+SharedDataCore::~SharedDataCore()
+{
+    if (valid_)
+    {
+        std::free(data_);
+        
+        valid_ = false;
+        data_ = nullptr;
+        size_ = 0;
+        id_ = 0;
+        counter_ = 0;
+    }
+}
+
+
+
+SharedDataCore::SharedDataCore(SharedDataCore&& other) noexcept
+: data_(other.data_), size_(other.size_), id_(other.id_), valid_(other.valid_), counter_(other.counter_)
+{
+    other.data_ = nullptr;
+    other.size_ = 0;
+    other.id_ = 0;
+    other.valid_ = false;
+    other.counter_ = 0;
+}
+
+
+
+SharedDataCore& SharedDataCore::operator=(SharedDataCore&& other) noexcept
+{
+    if (this != &other) {
+        data_ = other.data_;
+        size_ = other.size_;
+        id_ = other.id_;
+        valid_ = other.valid_;
+        counter_ = other.counter_;
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.id_ = 0;
+        other.valid_ = false;
+        other.counter_ = 0;
+    }
+    return *this;
+}
+
+
+
+bool SharedDataCore::valid()
+{
+    return valid_;
+}
+
+
+
+uint8_t* SharedDataCore::data()
+{
+    return data_;
+}
+
+
+
+uint64_t SharedDataCore::size()
+{
+    return size_;
+}
+
+
+
+uint64_t SharedDataCore::id()
+{
+    return id_;
+}
+
+
+
+uint64_t SharedDataCore::counter()
+{
+    return counter_;
+}
+
+
+
+void SharedDataCore::increaseCounter()
+{
+    ++counter_;
+}
+
+
+
+void SharedDataCore::decreaseCounter()
+{
+    if (counter_ > 0)
+    {
+        --counter_;
+    }
+}
+
+
+
+SharedDataCore SharedDataCore::allocate(uint64_t size, uint64_t id) noexcept
+{
+    void* data = std::malloc(size);
+    if (data)
+    {
+        return SharedDataCore((uint8_t*)data, size, id);
+    }
+    else
+    {
+        return SharedDataCore();
+    }
+}
