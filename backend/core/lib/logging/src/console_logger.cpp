596f1d360a4e68729ef5af23f1b54bf24bf6defe
Jakub Medek <medekjak@gmail.com>
2025-07-16 17:49:40 +0200

    Update console logger formatting.

--- backend/core/lib/logging/src/console_logger.cpp
+++ backend/core/lib/logging/src/console_logger.cpp
@@ -2,6 +2,8 @@
 
 #include <sstream>
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 using namespace aergo::core::logging;
 using namespace aergo::module::logging;
@@ -13,10 +15,10 @@ void ConsoleLogger::log(SourceType source_type, const char* source_name, uint64_
     switch (log_type)
     {
         case LogType::INFO:
-            log_stream << "[INFO]    ";
+            log_stream << "[INFO] ";
             break;
         case LogType::ERROR:
-            log_stream << "[ERROR]   ";
+            log_stream << "[ERROR] ";
             break;
         case LogType::WARNING:
             log_stream << "[WARNING] ";
@@ -26,20 +28,39 @@ void ConsoleLogger::log(SourceType source_type, const char* source_name, uint64_
             break;
     }
 
+    auto now = std::chrono::system_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    std::tm buf;
+    #if defined(_WIN32) || defined(_WIN64)
+        localtime_s(&buf, &in_time_t);       // Windows
+    #else
+        localtime_r(&in_time_t, &buf);       // POSIX
+    #endif
+
+    log_stream << std::put_time(&buf, "%Y/%m/%d %H:%M:%S")
+        << '.' << std::setw(3) << std::setfill('0') << ms.count() << " ";
+
     switch (source_type)
     {
         case SourceType::CORE:
-            log_stream << "(CORE) ";
+            if (source_name == nullptr || *source_name == '\0')
+                log_stream << "(CORE) ";
+            else
+                log_stream << "(CORE, " << source_name << ") ";
             break;
         case SourceType::MODULE:
-            log_stream << "(" << source_name << ", ID: " << source_module_id << ") ";
+            log_stream << "(" << (source_name ? source_name : "UNKNOWN_NAME") << ", ID: " << source_module_id << ") ";
             break;
         default:
             log_stream << "(UNKNOWN) ";
             break;
     }
 
-    log_stream << message;
+    if (message)
+    {
+        log_stream << message;
+    }
 
     std::cout << log_stream.str() << std::endl;
 }
