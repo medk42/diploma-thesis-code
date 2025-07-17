93efd4986d29496d33631889e121ccb5e394636a
Jakub Medek <medekjak@gmail.com>
2025-07-17 17:25:11 +0200

    Add static allocator test.

---
+++ backend/core/lib/memory_allocation/tests/src/static_allocator_test.cpp
@@ -0,0 +1,130 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "test_memory_allocator.h"
+#include "test_logger.h"
+#include "utils/memory_allocation/static_allocator.h"
+
+#include <algorithm>
+
+using namespace aergo::core::memory_allocation;
+
+
+
+TEST_CASE( "StaticAllocator", "[static_allocator]" )
+{
+    TestLogger logger;
+
+    uint64_t slot_size_bytes = 1000;
+    uint32_t number_of_slots = 100;
+
+    SECTION("broken allocator")
+    {
+        TestMemoryAllocator memory_allocator(false);
+        REQUIRE(logger.logs().size() == 0);
+        REQUIRE_THROWS_AS(StaticAllocator(slot_size_bytes, number_of_slots, &logger, &memory_allocator), StaticAllocator::StaticAllocatorInitializationException);
+        REQUIRE(logger.logs().size() == 1);
+    }
+    
+    SECTION("working allocator")
+    {
+        TestMemoryAllocator memory_allocator(true);
+        StaticAllocator static_allocator(slot_size_bytes, number_of_slots, &logger, &memory_allocator);
+
+        REQUIRE(logger.logs().size() == 0);
+        
+        std::vector<aergo::module::ISharedData*> allocated_data;
+        for (uint32_t i = 0; i < number_of_slots; ++i)
+        {
+            aergo::module::ISharedData* data;
+            REQUIRE_NOTHROW(data = static_allocator.allocateImpl());
+            REQUIRE(data != nullptr);
+            REQUIRE(data->valid());
+            REQUIRE(data->size() == slot_size_bytes);
+            REQUIRE(data->data() == (uint8_t*)((uint64_t)i + 1));
+            allocated_data.push_back(data);
+        }
+
+        {
+            aergo::module::ISharedData* data;
+            REQUIRE_NOTHROW(data = static_allocator.allocateImpl());
+            REQUIRE(data == nullptr);
+        }
+
+        REQUIRE(logger.logs().size() == 0);
+        aergo::module::ISharedData* data42 = allocated_data[42];
+        allocated_data.erase(allocated_data.begin() + 42);
+        REQUIRE_NOTHROW(static_allocator.removeOwnerImpl(data42));
+
+        aergo::module::ISharedData* data42new;
+        REQUIRE_NOTHROW(data42new = static_allocator.allocateImpl());
+        REQUIRE(data42 == data42new);
+        allocated_data.push_back(data42new);
+
+        aergo::module::ISharedData* data_null;
+        REQUIRE_NOTHROW(data_null = static_allocator.allocateImpl());
+        REQUIRE(data_null == nullptr);
+
+        for (uint32_t i = 0; i < number_of_slots; ++i)
+        {
+            REQUIRE_NOTHROW(static_allocator.addOwnerImpl(allocated_data[i]));
+            REQUIRE_NOTHROW(static_allocator.addOwnerImpl(allocated_data[i]));  
+        }
+
+        REQUIRE_NOTHROW(data_null = static_allocator.allocateImpl());
+        REQUIRE(data_null == nullptr);
+        
+        REQUIRE(logger.logs().size() == 0);
+
+        for (uint32_t i = 0; i < number_of_slots; ++i)
+        {
+            REQUIRE_NOTHROW(static_allocator.removeOwnerImpl(allocated_data[i]));
+        }
+
+        REQUIRE_NOTHROW(data_null = static_allocator.allocateImpl());
+        REQUIRE(data_null == nullptr);
+
+        for (uint32_t i = 0; i < 50; ++i)
+        {
+            REQUIRE_NOTHROW(static_allocator.removeOwnerImpl(allocated_data[i]));
+        }
+
+        REQUIRE_NOTHROW(data_null = static_allocator.allocateImpl());
+        REQUIRE(data_null != nullptr);
+        allocated_data.push_back(data_null);
+        
+        REQUIRE(logger.logs().size() == 0);
+
+        for (uint32_t i = 0; i < allocated_data.size(); ++i)
+        {
+            REQUIRE_NOTHROW(static_allocator.removeOwnerImpl(allocated_data[i]));
+        }
+        
+        REQUIRE(logger.logs().size() == 50);
+
+        allocated_data.clear();
+
+        for (uint32_t i = 0; i < number_of_slots; ++i)
+        {
+            aergo::module::ISharedData* data;
+            REQUIRE_NOTHROW(data = static_allocator.allocateImpl());
+            REQUIRE(data != nullptr);
+            REQUIRE(data->valid());
+            REQUIRE(data->size() == slot_size_bytes);
+            for (uint32_t j = 0; j < i; ++j)
+            {
+                REQUIRE(allocated_data.at(j)->data() != data->data());
+            }
+
+            allocated_data.push_back(data);
+        }
+
+        REQUIRE(logger.logs().size() == 50);
+
+        for (uint32_t i = 0; i < allocated_data.size(); ++i)
+        {
+            REQUIRE_NOTHROW(static_allocator.removeOwnerImpl(allocated_data[i]));
+        }
+        
+        REQUIRE(logger.logs().size() == 50);
+    }
+}
