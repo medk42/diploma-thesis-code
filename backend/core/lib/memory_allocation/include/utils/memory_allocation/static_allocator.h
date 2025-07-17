aac6dbec7aea97adfa72701bf24faf9c6ed319a0
Jakub Medek <medekjak@gmail.com>
2025-07-17 16:21:36 +0200

    Create the static allocator.

---
+++ backend/core/lib/memory_allocation/include/utils/memory_allocation/static_allocator.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "module_common/module_interface_threads.h"
+#include "utils/logging/logger.h"
+#include "shared_data_core.h"
+
+#include <vector>
+#include <deque>
+#include <set>
+#include <optional>
+#include <mutex>
+
+
+namespace aergo::core::memory_allocation
+{
+    class StaticAllocator : public aergo::module::IAllocatorCore
+    {
+    public:
+        class StaticAllocatorInitializationException : public std::exception {};
+
+        /// @throws StaticAllocatorInitializationException if data could not be allocated.
+        StaticAllocator(uint64_t slot_size_bytes, uint32_t number_of_slots, aergo::core::logging::ILogger* logger, IMemoryAllocator* custom_allocator);
+
+        StaticAllocator(const StaticAllocator& other) = delete;
+        StaticAllocator(StaticAllocator&& other) noexcept = default;
+
+        StaticAllocator& operator=(const StaticAllocator& other) = delete;
+        StaticAllocator& operator=(StaticAllocator&& other) noexcept = default;
+
+        /// @param number_of_bytes parameter is ignored, since we have fixed slots
+        virtual aergo::module::ISharedData* allocate(uint64_t number_of_bytes) noexcept override final;
+        virtual void addOwner(aergo::module::ISharedData* data) noexcept override final;
+        virtual void removeOwner(aergo::module::ISharedData* data) noexcept override final;
+
+        // separate for testing that it does not throw exceptions
+        aergo::module::ISharedData* allocateImpl();
+        void addOwnerImpl(aergo::module::ISharedData* data);
+        void removeOwnerImpl(aergo::module::ISharedData* data);
+
+    private:
+        void log(aergo::module::logging::LogType log_type, const char* message);
+
+        IMemoryAllocator* memory_allocator_;
+        DefaultAllocator default_memory_allocator_;
+        aergo::core::logging::ILogger* logger_;
+
+        std::vector<SharedDataCore> preallocated_data_;
+
+        std::deque<std::size_t> free_memory_slot_ids_;
+        std::set<std::size_t> allocated_memory_slots_; // for checking pointer validity in addOwnerImpl / removeOwnerImpl
+
+        std::mutex mutex_;
+    };
+}
---
+++ backend/core/lib/memory_allocation/src/static_allocator.cpp
@@ -0,0 +1,133 @@
+#include "utils/memory_allocation/static_allocator.h"
+
+using namespace aergo::core::memory_allocation;
+
+
+
+StaticAllocator::StaticAllocator(uint64_t slot_size_bytes, uint32_t number_of_slots, aergo::core::logging::ILogger* logger, IMemoryAllocator* custom_allocator = nullptr)
+: logger_(logger)
+{
+    if (custom_allocator)
+    {
+        memory_allocator_ = custom_allocator;
+    }
+    else
+    {
+        memory_allocator_ = &default_memory_allocator_;
+    }
+
+    preallocated_data_.reserve(number_of_slots);
+
+    for (uint32_t i = 0; i < number_of_slots; ++i)
+    {
+        preallocated_data_.emplace_back(std::move(SharedDataCore::allocate(memory_allocator_, slot_size_bytes, i)));
+        if (!preallocated_data_[i].valid())
+        {
+            log(aergo::module::logging::LogType::ERROR, "Failed to initialize StaticAllocator");
+            throw StaticAllocatorInitializationException();
+        }
+
+        free_memory_slot_ids_.push_back(i);
+    }
+}
+
+
+
+aergo::module::ISharedData* StaticAllocator::allocate(uint64_t number_of_bytes) noexcept { return allocateImpl(); }
+void StaticAllocator::addOwner(aergo::module::ISharedData* data) noexcept { addOwnerImpl(data); }
+void StaticAllocator::removeOwner(aergo::module::ISharedData* data) noexcept { removeOwnerImpl(data); }
+
+
+
+aergo::module::ISharedData* StaticAllocator::allocateImpl()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    if (free_memory_slot_ids_.empty())
+    {
+        return nullptr;
+    }
+
+    size_t free_id = free_memory_slot_ids_.front();
+    free_memory_slot_ids_.pop_front();
+
+    allocated_memory_slots_.insert((size_t)(&preallocated_data_[free_id]));
+
+    return &preallocated_data_[free_id];
+}
+
+
+
+void StaticAllocator::addOwnerImpl(aergo::module::ISharedData* data)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    if (!allocated_memory_slots_.contains((std::size_t)data))
+    {
+        log(aergo::module::logging::LogType::ERROR, "Attempting to add owner on invalid or unowned data.");
+        return;
+    }
+    
+    SharedDataCore* data_core = dynamic_cast<SharedDataCore*>(data);
+
+    if (data_core && data_core->valid())
+    {
+        data_core->increaseCounter();
+    }
+    else if (!data)
+    {
+        log(aergo::module::logging::LogType::ERROR, "Attempting to add owner to nullptr.");
+    }
+    else if (!data_core)
+    {
+        log(aergo::module::logging::LogType::ERROR, "Dynamic cast failed in add owner");
+    }
+    else if (!data_core->valid())
+    {
+        log(aergo::module::logging::LogType::ERROR, "Attempting to add owner to invalid data.");
+    }
+}
+
+
+
+void StaticAllocator::removeOwnerImpl(aergo::module::ISharedData* data)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+     if (!allocated_memory_slots_.contains((std::size_t)data))
+    {
+        log(aergo::module::logging::LogType::ERROR, "Attempting to remove owner from invalid or unowned data.");
+        return;
+    }
+
+    SharedDataCore* data_core = dynamic_cast<SharedDataCore*>(data);
+
+    if (data_core && data_core->valid())
+    {
+        data_core->decreaseCounter();
+        if (data_core->counter() == 0)
+        {
+            free_memory_slot_ids_.push_back(data_core->id());
+            allocated_memory_slots_.erase((std::size_t)data);
+        }
+    }
+    else if (!data)
+    {
+        log(aergo::module::logging::LogType::ERROR, "Attempting to remove owner from nullptr.");
+    }
+    else if (!data_core)
+    {
+        log(aergo::module::logging::LogType::ERROR, "Dynamic cast failed in remove owner.");
+    }
+    else if (!data_core->valid())
+    {
+        log(aergo::module::logging::LogType::ERROR, "Attempting to remove owner from invalid data.");
+    }
+}
+
+
+
+void StaticAllocator::log(aergo::module::logging::LogType log_type, const char* message)
+{
+    logger_->log(aergo::core::logging::SourceType::CORE, "StaticAllocator", 0, log_type, message);
+}
