ef2dd91a28c3d1f578f5c9322bbcf5be6d48044d
Jakub Medek <medekjak@gmail.com>
2025-07-23 16:22:46 +0200

    Specify logger parameters.

--- backend/core/lib/logging/include/utils/logging/logger.h
+++ backend/core/lib/logging/include/utils/logging/logger.h
@@ -13,8 +13,8 @@ namespace aergo::core::logging
 
         /// @brief Log a message.
         /// @param source_type Core or Module.
-        /// @param source_name For module use module name, for core use name of part of core (memory allocation for example).
-        /// @param source_module_id Only for module, ID module received from core.
+        /// @param source_name For module use module name, for core use name of part of core (memory allocation for example). Use nullptr for no source.
+        /// @param source_module_id Only for module, ID module received from core. Use 0 for other source types.
         /// @param log_type Info, warning or error.
         /// @param message Log message.
         virtual void log(SourceType source_type, const char* source_name, uint64_t source_module_id, aergo::module::logging::LogType log_type, const char* message) = 0;
