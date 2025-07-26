0a6761ed537c08ca6371567b440de5f8facbe5b5
Jakub Medek <medekjak@gmail.com>
2025-07-26 20:43:34 +0200

    Implement access to running modules, state id of running modules (changes after each create/destroy), create a map and prompt to read existing publish/response channels. Add addModule implementation. Refactor.

--- backend/core/lib/core/include/core/core.h
+++ backend/core/lib/core/include/core/core.h
@@ -4,6 +4,8 @@
 #include "utils/logging/logger.h"
 #include "core_structures.h"
 
+#include <map>
+
 namespace aergo::core
 {
     class Core : public aergo::module::ICore
@@ -16,6 +18,7 @@ namespace aergo::core
         /// Only call this method once. Subsequent calls are ignored.
         void initialize(const char* modules_dir, const char* data_dir);
 
+        // TODO don't forget, these will be called ASYNC from modules, they need lock-free operation or synchronization
         virtual void sendMessage(aergo::module::ChannelIdentifier source_channel, aergo::module::message::MessageHeader message) noexcept override final;
         virtual void sendResponse(aergo::module::ChannelIdentifier source_channel, aergo::module::ChannelIdentifier target_channel, aergo::module::message::MessageHeader message) noexcept override final;
         virtual void sendRequest(aergo::module::ChannelIdentifier source_channel, aergo::module::ChannelIdentifier target_channel, aergo::module::message::MessageHeader message) noexcept override final;
@@ -23,19 +26,62 @@ namespace aergo::core
         virtual aergo::module::IAllocatorCore* createBufferAllocator(uint64_t slot_size_bytes, uint32_t number_of_slots) noexcept override final;
         virtual void deleteAllocator(aergo::module::IAllocatorCore* allocator) noexcept override final;
 
-        const aergo::module::ModuleInfo* getLoadedModulesInfo(size_t loaded_module_id);
-        size_t getLoadedModulesCount() const;
+        /// @return nullptr if out of range
+        const aergo::module::ModuleInfo* getLoadedModulesInfo(uint64_t loaded_module_id);
+        uint64_t getLoadedModulesCount() const;
+
+        /// @return nullptr if out of range or module with specified ID was destroyed
+        structures::ModuleData* getRunningModulesInfo(uint64_t running_module_id);
+        uint64_t getRunningModulesCount();
+
+        /// @brief ID of the module mapping state. ID changes when modules get created or destroyed.
+        uint64_t getModulesMappingStateId();
+
+        /// @brief Get existing publish channels for specified channel type identifier. 
+        /// @return Returns a list of modules and channels inside the modules or std::nullopt if specified identifier is not tied to any channels yet.
+        std::optional<std::vector<aergo::module::ChannelIdentifier>&> getExistingPublishChannels(const char* channel_type_identifier);
+
+        /// @brief Get existing response channels for specified channel type identifier.
+        /// @return Returns a list of modules and channels inside the modules or std::nullopt if specified identifier is not tied to any channels yet.
+        std::optional<std::vector<aergo::module::ChannelIdentifier>&> getExistingResponseChannels(const char* channel_type_identifier);
+
+        /// @brief Return module specified by ID. Module will only be removed if it exists (id < getRunningModulesCount() and wasn't yet removed)
+        /// and it does not have dependencies (modules connected to its outputs). If it has dependencies and recursive is true, module and all 
+        /// of its (recursive) dependencies will be removed. 
+        /// @param id id of the module to remove
+        /// @param recursive remove all dependencies too, if module has dependencies and recursive is false, module will not be removed and method will return false
+        /// @return true if module (and possibly dependencies, if recursive is true) was removed, false otherwise 
+        /// (module with id does not exist, was already removed or has dependencies if recursive is false).
+        bool removeModule(uint64_t id, bool recursive);
+
+        /// @brief Attempt to create a new module. Creation fails if loaded_module_id is out of range of loaded modules,
+        /// channel_map_info input mapping does not match the module's creation requirements or the createModule call returned
+        /// nullptr.
+        /// @param loaded_module_id ID of the module to add.
+        /// @param channel_map_info Communication mapping.
+        /// @return true on success (module added), false otherwise (module not added)
+        bool addModule(uint64_t loaded_module_id, aergo::module::InputChannelMapInfo channel_map_info);
 
     private:
         void log(aergo::module::logging::LogType log_type, const char* message);
         void loadModules(const char* modules_dir, const char* data_dir);
         void autoCreateModules();
         uint64_t getNextModuleId();
+        void registerModule(uint64_t module_id, const aergo::module::ModuleInfo* module_info);  // register to existing_publish_channels_ and existing_response_channels_
+        bool checkChannelMapValidity(aergo::module::InputChannelMapInfo channel_map_info, const aergo::module::ModuleInfo* module_info); // return true if channel_map_info matched module_info
+        
+        /// @brief Attempt to create and start module identified by loaded_module_id.
+        /// @return true on success, false on failure
+        bool createAndStartModule(uint64_t loaded_module_id, aergo::module::InputChannelMapInfo channel_map_info, uint32_t module_thread_timeout_ms); 
 
 
         bool initialized_;
         std::vector<structures::ModuleLoaderData> loaded_modules_;
         std::vector<std::unique_ptr<structures::ModuleData>> running_modules_;
+        uint64_t module_mapping_state_id_;
+
+        std::map<std::string, std::vector<aergo::module::ChannelIdentifier>> existing_publish_channels_;
+        std::map<std::string, std::vector<aergo::module::ChannelIdentifier>> existing_response_channels_;
 
         logging::ILogger* logger_;
     };
--- backend/core/lib/core/src/core.cpp
+++ backend/core/lib/core/src/core.cpp
@@ -6,7 +6,7 @@ using namespace aergo::core;
 
 
 Core::Core(logging::ILogger* logger)
-: logger_(logger), initialized_(false) {}
+: logger_(logger), initialized_(false), module_mapping_state_id_(0) {}
 
 
 
@@ -98,54 +98,78 @@ void Core::autoCreateModules()
     {
         if (loaded_modules_[i]->readModuleInfo()->auto_create_)
         {
-
-            const char* data_path;
-            if (std::filesystem::exists(loaded_modules_[i].getModuleDataPath()))
+            if (createAndStartModule(i, empty_channel_info, defaults::module_thread_timeout_ms_))
             {
-                data_path = loaded_modules_[i].getModuleDataPath().c_str();
+                std::string success_message = std::string("Successfully auto-created module: ") + loaded_modules_[i].getModuleUniqueName();
+                log(aergo::module::logging::LogType::INFO, success_message.c_str());
             }
             else
             {
-                data_path = nullptr;
+                std::string success_message = std::string("Failed to auto-create module: ") + loaded_modules_[i].getModuleUniqueName();
+                log(aergo::module::logging::LogType::WARNING, success_message.c_str());
             }
+        }
+    }
+}
 
-            
-            uint64_t next_module_id = getNextModuleId();
-            auto module_data = std::make_unique<structures::ModuleData>( // TODO this may be an issue, module_ does not have a default constructor maybe
-                structures::ModuleLogger(
-                    logger_, 
-                    loaded_modules_[i].getModuleUniqueName(), 
-                    next_module_id
-                ), 
-                &(loaded_modules_[i])
-            );
 
-            ModuleLoader::ModulePtr created_module(loaded_modules_[i]->createModule(data_path, this, empty_channel_info, &(module_data->logger_), next_module_id));
 
-            if (created_module.get() == nullptr)
-            {
-                std::string error_message = std::string("Failed to create module (createModule call failed) for module: ") + module_data->module_loader_data_->getModuleUniqueName();
-                log(aergo::module::logging::LogType::WARNING, error_message.c_str());
-            }
-            else
-            {
-                bool result = created_module->threadStart(defaults::module_thread_timeout_ms_);
-                if (!result)
-                {
-                    bool result2 = created_module->threadStop(defaults::module_thread_timeout_ms_);
+bool Core::createAndStartModule(uint64_t loaded_module_id, aergo::module::InputChannelMapInfo channel_map_info, uint32_t module_thread_timeout_ms)
+{
+    if (loaded_module_id >= loaded_modules_.size())
+    {
+        return false;
+    }
 
-                    std::string error_message = std::string("Failed to start thread for module: \"") + module_data->module_loader_data_->getModuleUniqueName() + std::string("\", stop success: ") + (result2 ? "TRUE" : "false");
-                    log(aergo::module::logging::LogType::WARNING, error_message.c_str());
-                }
-                else
-                {
-                    module_data->module_ = std::move(created_module);
-                    running_modules_.push_back(std::move(module_data));
+    const char* data_path;
+    if (std::filesystem::exists(loaded_modules_[loaded_module_id].getModuleDataPath()))
+    {
+        data_path = loaded_modules_[loaded_module_id].getModuleDataPath().c_str();
+    }
+    else
+    {
+        data_path = nullptr;
+    }
 
-                    std::string success_message = std::string("Successfully auto-created module: ") + module_data->module_loader_data_->getModuleUniqueName();
-                    log(aergo::module::logging::LogType::INFO, success_message.c_str());
-                }
-            }
+    
+    uint64_t next_module_id = getNextModuleId();
+    auto module_data = std::make_unique<structures::ModuleData>(
+        structures::ModuleLogger(
+            logger_, 
+            loaded_modules_[loaded_module_id].getModuleUniqueName(), 
+            next_module_id
+        ), 
+        &(loaded_modules_[loaded_module_id])
+    );
+
+    ModuleLoader::ModulePtr created_module(loaded_modules_[loaded_module_id]->createModule(data_path, this, channel_map_info, &(module_data->logger_), next_module_id));
+
+    if (created_module.get() == nullptr)
+    {
+        std::string error_message = std::string("Failed to create module (createModule call failed) for module: ") + module_data->module_loader_data_->getModuleUniqueName();
+        log(aergo::module::logging::LogType::WARNING, error_message.c_str());
+        return false;
+    }
+    else
+    {
+        bool result = created_module->threadStart(module_thread_timeout_ms);
+        if (!result)
+        {
+            bool result2 = created_module->threadStop(module_thread_timeout_ms);
+
+            std::string error_message = std::string("Failed to start thread for module: \"") + module_data->module_loader_data_->getModuleUniqueName() + std::string("\", stop success: ") + (result2 ? "TRUE" : "false");
+            log(aergo::module::logging::LogType::WARNING, error_message.c_str());
+
+            return false;
+        }
+        else
+        {
+            module_data->module_ = std::move(created_module);
+            running_modules_.push_back(std::move(module_data));
+
+            registerModule(next_module_id, loaded_modules_[loaded_module_id]->readModuleInfo());
+
+            return true;
         }
     }
 }
@@ -159,6 +183,27 @@ uint64_t Core::getNextModuleId()
 
 
 
+void Core::registerModule(uint64_t module_id, const aergo::module::ModuleInfo* module_info)
+{
+    for (uint32_t channel_id = 0; channel_id < module_info->publish_producer_count_; ++channel_id)
+    {
+        existing_publish_channels_[module_info->publish_producers_[channel_id].channel_type_identifier_].push_back({
+            .producer_module_id_ = module_id,
+            .producer_channel_id_ = channel_id
+        });
+    }
+
+    for (uint32_t channel_id = 0; channel_id < module_info->response_producer_count_; ++channel_id)
+    {
+        existing_response_channels_[module_info->response_producers_[channel_id].channel_type_identifier_].push_back({
+            .producer_module_id_ = module_id,
+            .producer_channel_id_ = channel_id
+        });
+    }
+}
+
+
+
 void Core::log(aergo::module::logging::LogType log_type, const char* message)
 {
     logger_->log(logging::SourceType::CORE, nullptr, 0, log_type, message);
@@ -166,7 +211,7 @@ void Core::log(aergo::module::logging::LogType log_type, const char* message)
 
 
 
-const aergo::module::ModuleInfo* Core::getLoadedModulesInfo(size_t loaded_module_id)
+const aergo::module::ModuleInfo* Core::getLoadedModulesInfo(uint64_t loaded_module_id)
 {
     if (loaded_module_id < loaded_modules_.size())
     {
@@ -180,9 +225,164 @@ const aergo::module::ModuleInfo* Core::getLoadedModulesInfo(size_t loaded_module
 
 
 
-size_t Core::getLoadedModulesCount() const
+uint64_t Core::getLoadedModulesCount() const
 {
-    return loaded_modules_.size();
+    return (uint64_t)loaded_modules_.size();
+}
+
+
+
+structures::ModuleData* Core::getRunningModulesInfo(uint64_t running_module_id)
+{
+    if (running_module_id < running_modules_.size())
+    {
+        return running_modules_[running_module_id].get();
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+
+
+uint64_t Core::getRunningModulesCount()
+{
+    return (uint64_t)running_modules_.size();
+}
+
+
+
+uint64_t Core::getModulesMappingStateId()
+{
+    return module_mapping_state_id_;
+}
+
+
+
+std::optional<std::vector<aergo::module::ChannelIdentifier>&> Core::getExistingPublishChannels(const char* channel_type_identifier)
+{
+    if (existing_publish_channels_.contains(channel_type_identifier))
+    {
+        return existing_publish_channels_[channel_type_identifier];
+    }
+    else
+    {
+        return std::nullopt;
+    }
+}
+
+
+
+std::optional<std::vector<aergo::module::ChannelIdentifier>&> Core::getExistingResponseChannels(const char* channel_type_identifier)
+{
+    if (existing_response_channels_.contains(channel_type_identifier))
+    {
+        return existing_response_channels_[channel_type_identifier];
+    }
+    else
+    {
+        return std::nullopt;
+    }
+}
+
+
+
+bool Core::removeModule(uint64_t id, bool recursive)
+{
+    // TODO remove module, dependencies and all existing mappings (existing_response_channels_ and publish)
+}
+
+
+
+bool Core::addModule(uint64_t loaded_module_id, aergo::module::InputChannelMapInfo channel_map_info)
+{
+    if (loaded_module_id >= loaded_modules_.size())
+    {
+        return false;
+    }
+
+    if (!checkChannelMapValidity(channel_map_info, loaded_modules_[loaded_module_id]->readModuleInfo()))
+    {
+        return false;
+    }
+
+    bool res = createAndStartModule(loaded_module_id, channel_map_info, defaults::module_thread_timeout_ms_);
+    if (res)
+    {
+        ++module_mapping_state_id_;
+    }
+
+    return res;
+}
+
+
+
+bool Core::checkChannelMapValidity(aergo::module::InputChannelMapInfo channel_map_info, const aergo::module::ModuleInfo* module_info)
+{
+    if (module_info->subscribe_consumer_count_ != channel_map_info.subscribe_consumer_info_count_)
+    {
+        return false;
+    }
+
+    if (module_info->request_consumer_count_ != channel_map_info.request_consumer_info_count_)
+    {
+        return false;
+    }
+
+    for (uint32_t subscriber_id = 0; subscriber_id < channel_map_info.subscribe_consumer_info_count_; ++subscriber_id)
+    {
+        switch (module_info->subscribe_consumers_[subscriber_id].count_)
+        {
+        case aergo::module::communication_channel::SubscribeConsumer::Count::SINGLE:
+            if (channel_map_info.subscribe_consumer_info_[subscriber_id].channel_identifier_count_ != 1)
+            {
+                return false;
+            }
+            break;
+        case aergo::module::communication_channel::SubscribeConsumer::Count::RANGE:
+            if (channel_map_info.subscribe_consumer_info_[subscriber_id].channel_identifier_count_ < module_info->subscribe_consumers_[subscriber_id].min_
+             || channel_map_info.subscribe_consumer_info_[subscriber_id].channel_identifier_count_ > module_info->subscribe_consumers_[subscriber_id].max_)
+            {
+                return false;
+            }
+            break;
+        case aergo::module::communication_channel::SubscribeConsumer::Count::AUTO_ALL:
+            break;
+        default:
+            log(aergo::module::logging::LogType::ERROR, "Unexpected enum type in checkChannelMapValidity, terminating!");
+            std::terminate();
+            break;
+        }
+    }
+
+    for (uint32_t request_id = 0; request_id < channel_map_info.request_consumer_info_count_; ++request_id)
+    {
+        switch (module_info->request_consumers_[request_id].count_)
+        {
+        case aergo::module::communication_channel::RequestConsumer::Count::SINGLE:
+            if (channel_map_info.request_consumer_info_[request_id].channel_identifier_count_ != 1)
+            {
+                return false;
+            }
+            break;
+        case aergo::module::communication_channel::RequestConsumer::Count::RANGE:
+            if (channel_map_info.request_consumer_info_[request_id].channel_identifier_count_ < module_info->request_consumers_[request_id].min_
+             || channel_map_info.request_consumer_info_[request_id].channel_identifier_count_ > module_info->request_consumers_[request_id].max_)
+            {
+                return false;
+            }
+            break;
+        case aergo::module::communication_channel::RequestConsumer::Count::AUTO_ALL:
+            break;
+        default:
+            log(aergo::module::logging::LogType::ERROR, "Unexpected enum type in checkChannelMapValidity, terminating!");
+            std::terminate();
+            break;
+        }
+    }
+
+    return true;
 }
 
 
