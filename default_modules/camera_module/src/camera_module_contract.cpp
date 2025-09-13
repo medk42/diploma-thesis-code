fc8376afd31054bcf48e7da093b3786cecb203e2
Jakub Medek <medekjak@gmail.com>
2025-09-13 02:46:00 +0200

    Update camera module to use activation wrapper for testing.

--- default_modules/camera_module/src/camera_module_contract.cpp
+++ default_modules/camera_module/src/camera_module_contract.cpp
@@ -3,6 +3,9 @@
 #include "module_common/module_contract.h"
 #include "module_common/dll_module_wrapper.h"
 
+#include "module_helpers/activation_wrapper/message_types.h"
+#include "module_helpers/activation_wrapper/activation_wrapper.h"
+
 
 #define MODULE_A_API_VERSION 2
 
@@ -20,19 +23,27 @@ static constexpr communication_channel::Producer camera_module_publish_producers
     }
 };
 
+static constexpr communication_channel::Producer camera_module_response_producers[] = {
+    aergo::module::helpers::activation_wrapper::message_types::activation_response_producer
+};
+
 static constexpr ModuleInfo module_info = {
     .display_name_ = "Camera Input Module",
     .display_description_ = "Module captures raw camera data from a connected camera using OpenCV and publishes it as BGR image frames.",
     .publish_producers_ = camera_module_publish_producers,
     .publish_producer_count_ = std::size(camera_module_publish_producers),
-    .response_producers_ = nullptr,
-    .response_producer_count_ = 0,
+    .response_producers_ = camera_module_response_producers,
+    .response_producer_count_ = std::size(camera_module_response_producers),
     .subscribe_consumers_ = nullptr,
     .subscribe_consumer_count_ = 0,
     .request_consumers_ = nullptr,
     .request_consumer_count_ = 0,
     .auto_create_ = false
 };
+
+static aergo::module::helpers::activation_wrapper::params::ParameterList parameters(std::vector<aergo::module::helpers::activation_wrapper::params::ParameterDescription>{
+
+});
 
 
 const ModuleInfo* readModuleInfo()
@@ -43,14 +54,14 @@ const ModuleInfo* readModuleInfo()
 aergo::module::dll::IDllModule* createModule(const char* data_path, ICore* core, InputChannelMapInfo channel_map_info, logging::ILogger* logger, uint64_t module_id)
 {
     auto module = std::make_unique<aergo::default_modules::camera_module::CameraModule>(data_path, core, channel_map_info, logger, module_id);
-    if (module->valid())
-    {
-        return new aergo::module::dll::DllModuleWrapper(std::move(module), &module_info, core, module_id, logger);
-    }
-    else
-    {
+    if (!module->valid())
         return nullptr;
-    }
+
+    auto wrapped_module = std::make_unique<aergo::module::helpers::activation_wrapper::ActivationWrapper>(std::move(module), module_info, &parameters);
+    if (!wrapped_module->valid())
+        return nullptr;
+
+    return new aergo::module::dll::DllModuleWrapper(std::move(wrapped_module), &module_info, core, module_id, logger);
 }
 
 void destroyModule(aergo::module::dll::IDllModule* module)
