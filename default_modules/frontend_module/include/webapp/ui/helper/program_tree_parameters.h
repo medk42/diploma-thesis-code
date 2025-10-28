984715c2e233875e4b388a4bebc07d09748da5b4
Jakub Medek <medekjak@gmail.com>
2025-10-28 14:54:55 +0100

    Implement UI wiring for ProgramTreeParameters class.

--- default_modules/frontend_module/include/webapp/ui/helper/program_tree_parameters.h
+++ default_modules/frontend_module/include/webapp/ui/helper/program_tree_parameters.h
@@ -1,15 +1,21 @@
 #pragma once
 
-#include "tab_selector.h"
 #include "right_module_parameter.h"
+#include "button.h"
+#include "input_fields.h"
+#include "parameter_value.h"
 
 #include "module_helpers/parameter_description/parameter_description.h"
+#include "module_helpers/usecase_tree/structs.h"
 
 #include <Wt/WContainerWidget.h>
 
 namespace aergo::default_modules::frontend_module::webapp::ui::helper
 {
     namespace p_desc = aergo::module::helpers::parameter_description;
+    namespace u_tree = aergo::module::helpers::usecase_tree;
+
+    using command_param_type = u_tree::structs::ExistingCommand::ParamType;
 
     class ProgramTreeParameters : public Wt::WContainerWidget
     {
@@ -20,7 +26,37 @@ namespace aergo::default_modules::frontend_module::webapp::ui::helper
             const std::vector<p_desc::ParameterDescription>& advanced_parameters
         );
 
+        void setConfirmEnable(bool enabled) { confirm_button_->setEnabled(enabled); }
+        bool isConfirmEnabled() const { return confirm_button_->isEnabled(); }
+        Wt::Signal<>& confirmClicked() { return confirm_button_->clicked(); }
+
+        bool setAllValues(const std::vector<std::vector<value_opt_t>>& auto_values,
+                          const std::vector<std::vector<value_opt_t>>& required_values,
+                          const std::vector<std::vector<value_opt_t>>& advanced_values);
+
+        // returns true if set (successful), false if not (invalid index or value not valid for parameter)
+        // mainly for use with ExistingCommand - if ExistingCommand does not accept the value from the signal, 
+        // this is called to reset the parameter widget value as well
+        bool setValue(command_param_type param_type, size_t param_index, size_t list_index, const value_opt_t& value);
+
+        Wt::Signal<const std::string&, const std::string&>& onShowDescription() { return onShowDescriptionClicked_; } // parameter title and description
+        Wt::Signal<command_param_type, size_t, size_t, value_opt_t>& onValueAdded() { return onValueAdded_; } // parameter list type, param index, list index, optionally value (if exists)
+        Wt::Signal<command_param_type, size_t, size_t>& onValueRemoved() { return onValueRemoved_; } // parameter list type, param index, list index
+        Wt::Signal<command_param_type, size_t, size_t, value_t>& onValueChanged() { return onValueChanged_; } // parameter list type, param index, list index, value
+    
+
     private:
-        TabSelector* tab_selector_{ nullptr };
+        Wt::WContainerWidget* setupSection(command_param_type section_type, Wt::WContainerWidget* section_container, const std::vector<p_desc::ParameterDescription>& parameters, const std::string& section_title, bool top_padding);
+
+        Button* confirm_button_ = nullptr;
+
+        std::vector<RightModuleParameter*> auto_parameters_;
+        std::vector<RightModuleParameter*> required_parameters_;
+        std::vector<RightModuleParameter*> advanced_parameters_;
+
+        Wt::Signal<const std::string&, const std::string&> onShowDescriptionClicked_; // parameter title and description
+        Wt::Signal<command_param_type, size_t, size_t, value_opt_t> onValueAdded_; // parameter list type, param index, optionally value (if exists)
+        Wt::Signal<command_param_type, size_t, size_t> onValueRemoved_; // parameter list type, param index, list index
+        Wt::Signal<command_param_type, size_t, size_t, value_t> onValueChanged_; // parameter list type, param index, list index, value
     };
 }
--- default_modules/frontend_module/src/program_tree_parameters.cpp
+++ default_modules/frontend_module/src/program_tree_parameters.cpp
@@ -1,7 +1,6 @@
 #include "webapp/ui/helper/program_tree_parameters.h"
 
 #include <Wt/WText.h>
-#include <Wt/WStackedWidget.h>
 
 using namespace aergo::default_modules::frontend_module::webapp::ui::helper;
 
@@ -13,32 +12,181 @@ ProgramTreeParameters::ProgramTreeParameters(
 {
     setStyleClass("program-tree-parameters");
 
-    auto parameter_section = addWidget(std::make_unique<Wt::WStackedWidget>());
+    auto parameter_section = addWidget(std::make_unique<Wt::WContainerWidget>());
     parameter_section->setStyleClass("program-tree-parameter-section");
-    auto required_parameters_section = parameter_section->addWidget(std::make_unique<Wt::WContainerWidget>());
-    required_parameters_section->setStyleClass("parameter-list");
-    auto advanced_parameters_section = parameter_section->addWidget(std::make_unique<Wt::WContainerWidget>());
-    advanced_parameters_section->setStyleClass("parameter-list");
-    parameter_section->setCurrentIndex(0);
+
+
+    auto auto_parameters_section = setupSection(command_param_type::AUTO, parameter_section, auto_parameters, "Automatic Parameters", false);
+    auto required_parameters_section = setupSection(command_param_type::REQUIRED, parameter_section, required_parameters, "Required Parameters", true);
+    auto advanced_parameters_section = setupSection(command_param_type::ADVANCED, parameter_section, advanced_parameters, "Advanced Parameters", true);
+
+
+    auto action_bar = addWidget(std::make_unique<Wt::WContainerWidget>());
+    action_bar->setStyleClass("action-bar");
     
-    for (const auto& param : auto_parameters)
-    {
-        required_parameters_section->addWidget(std::make_unique<RightModuleParameter>(param));
-    }
-    for (const auto& param : required_parameters)
-    {
-        required_parameters_section->addWidget(std::make_unique<RightModuleParameter>(param));
-    }
+    auto checkbox_container = action_bar->addWidget(std::make_unique<Wt::WContainerWidget>());
+    auto checkbox_label = checkbox_container->addWidget(std::make_unique<Wt::WText>("ADVANCED"));
+    checkbox_label->setStyleClass("light-gray-text checkbox-label");
+    auto checkbox = checkbox_container->addWidget(std::make_unique<ToggleCheckbox>());
+    checkbox->addStyleClass("input-checkbox-inline");
 
-    for (const auto& param : advanced_parameters)
-    {
-        advanced_parameters_section->addWidget(std::make_unique<RightModuleParameter>(param));
-    }
+    action_bar->addWidget(std::make_unique<Button>("Confirm", ButtonStyle::Primary));
 
-    tab_selector_ = addWidget(std::make_unique<TabSelector>(std::vector<std::string>{"Required", "Advanced"}));
-    tab_selector_->setSelectedTab(0);
 
-    tab_selector_->onTabSelected().connect([parameter_section](size_t index) {
-        parameter_section->setCurrentIndex(index);
+    advanced_parameters_section->setHidden(true);
+    checkbox->clicked().connect([advanced_parameters_section, checkbox]() {
+        bool is_checked = checkbox->isChecked();
+        advanced_parameters_section->setHidden(!is_checked);
     });
+}
+
+
+Wt::WContainerWidget* ProgramTreeParameters::setupSection(command_param_type section_type, Wt::WContainerWidget* section_container, const std::vector<p_desc::ParameterDescription>& parameters, const std::string& section_title, bool top_padding)
+{    
+    auto section = section_container->addWidget(std::make_unique<Wt::WContainerWidget>());
+    section->setStyleClass("parameter-list");
+
+    if (top_padding)
+    {
+        auto spacer = section->addWidget(std::make_unique<Wt::WContainerWidget>());
+        spacer->setStyleClass("parameter-section-spacer");
+    }
+
+    auto title = section->addWidget(std::make_unique<Wt::WText>(section_title));
+    title->setStyleClass("light-gray-text parameter-section-title");
+
+    for (size_t param_id = 0; param_id < parameters.size(); ++param_id)
+    {
+        auto single_param = section->addWidget(std::make_unique<RightModuleParameter>(parameters[param_id]));
+        single_param->onShowDescription().connect([this](const std::string& title, const std::string& description) {
+            onShowDescriptionClicked_.emit(title, description);
+        });
+        single_param->onValueAdded().connect([this, section_type, param_id](size_t list_index, const p_desc::ParameterValueOpt& value) {
+            onValueAdded_.emit(section_type, param_id, list_index, value);
+        });
+        single_param->onValueRemoved().connect([this, section_type, param_id](size_t list_index) {
+            onValueRemoved_.emit(section_type, param_id, list_index);
+        });
+        single_param->onValueChanged().connect([this, section_type, param_id](size_t list_index, const p_desc::ParameterValue& value) {
+            onValueChanged_.emit(section_type, param_id, list_index, value);
+        });
+
+        switch (section_type)
+        {
+            case command_param_type::AUTO:
+                auto_parameters_.push_back(single_param);
+                break;
+            case command_param_type::REQUIRED:
+                required_parameters_.push_back(single_param);
+                break;
+            case command_param_type::ADVANCED:
+                advanced_parameters_.push_back(single_param);
+                break;
+        }
+    }
+
+    return section;
+}
+
+
+
+bool ProgramTreeParameters::setValue(command_param_type param_type, size_t param_index, size_t list_index, const value_opt_t& value)
+{
+    RightModuleParameter* parameter_widget = nullptr;
+
+    switch (param_type)
+    {
+        case command_param_type::AUTO:
+            if (param_index < auto_parameters_.size())
+                parameter_widget = auto_parameters_[param_index];
+            break;
+        case command_param_type::REQUIRED:
+            if (param_index < required_parameters_.size())
+                parameter_widget = required_parameters_[param_index];
+            break;
+        case command_param_type::ADVANCED:
+            if (param_index < advanced_parameters_.size())
+                parameter_widget = advanced_parameters_[param_index];
+            break;
+    }
+
+    if (parameter_widget)
+    {
+        if (value)
+        {
+            return parameter_widget->setValue(*value, list_index);
+        }
+        else
+        {
+            parameter_widget->resetValue(list_index);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+bool ProgramTreeParameters::setAllValues(const std::vector<std::vector<value_opt_t>>& auto_values,
+                                        const std::vector<std::vector<value_opt_t>>& required_values,
+                                        const std::vector<std::vector<value_opt_t>>& advanced_values)
+{
+    auto set_values = [](RightModuleParameter* parameter_widget, const std::vector<value_opt_t>& values) -> bool {
+        for (size_t i = parameter_widget->listSize(); i < values.size(); ++i) // grow list if needed
+        {
+            if (!parameter_widget->addListItem())
+                return false; // could not add item
+        }
+        for (size_t i = parameter_widget->listSize(); i > values.size(); --i) // shrink list if needed
+        {
+            if (!parameter_widget->removeListItem(i - 1))
+                return false; // could not remove item
+        }
+        for (size_t i = 0; i < values.size(); ++i)
+        {
+            if (values[i])
+            {
+                if (!parameter_widget->setValue(*values[i], i))
+                    return false; // could not set value
+            }
+            else
+            {
+                parameter_widget->resetValue(i);
+            }
+        }
+        return true;
+    };
+
+    if (auto_values.size() != auto_parameters_.size() ||
+        required_values.size() != required_parameters_.size() ||
+        advanced_values.size() != advanced_parameters_.size())
+    {
+        return false;
+    }
+
+    for (size_t i = 0; i < auto_parameters_.size(); ++i)
+    {
+        if (!set_values(auto_parameters_[i], auto_values[i]))
+        {
+            return false;
+        }
+    }
+
+    for (size_t i = 0; i < required_parameters_.size(); ++i)
+    {
+        if (!set_values(required_parameters_[i], required_values[i]))
+        {
+            return false;
+        }
+    }
+
+    for (size_t i = 0; i < advanced_parameters_.size(); ++i)
+    {
+        if (!set_values(advanced_parameters_[i], advanced_values[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
