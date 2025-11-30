b9dd80f3f4fe7150592c14eeeb37d25fb8c536cf
Jakub Medek <medekjak@gmail.com>
2025-11-30 23:55:06 +0100

    Update message types definitions for robot interface (req/resp have to have the same identifier, add consumers for publishing)

--- backend/module_helpers/robot_interface/definitions_include/module_helpers/robot_interface/message_types_definitions.h
+++ backend/module_helpers/robot_interface/definitions_include/module_helpers/robot_interface/message_types_definitions.h
@@ -6,7 +6,7 @@ namespace aergo::module::helpers::robot_interface
 {
     constexpr aergo::module::communication_channel::Consumer robot_interface_request_consumer = {
         .count_ = aergo::module::communication_channel::Consumer::Count::SINGLE,
-        .channel_type_identifier_ = "robot_interface_req/v1:struct{req_type:uint8,feature:uint64,action_id:uint64}[+blob{data}]",
+        .channel_type_identifier_ = "robot_interface_req/v1:struct{req_type:uint8,feature:uint64,action_id:uint64}[+blob{data}];robot_interface_resp/v1:struct{resp_type:uint8,finished:bool,action_id:uint64}[+blob{data}]",
         .display_name_ = "Robot Control Request",
         .display_description_ = "Request channel for robot control.",
         .prioritized_ = true,
@@ -14,7 +14,7 @@ namespace aergo::module::helpers::robot_interface
     };
 
     constexpr aergo::module::communication_channel::Producer robot_interface_response_producer = {
-        .channel_type_identifier_ = "robot_interface_resp/v1:struct{resp_type:uint8,finished:bool,action_id:uint64}[+blob{data}]",
+        .channel_type_identifier_ = "robot_interface_req/v1:struct{req_type:uint8,feature:uint64,action_id:uint64}[+blob{data}];robot_interface_resp/v1:struct{resp_type:uint8,finished:bool,action_id:uint64}[+blob{data}]",
         .display_name_ = "Robot Control Response",
         .display_description_ = "Response channel for robot control actions.",
         .prioritized_ = true,
@@ -24,15 +24,29 @@ namespace aergo::module::helpers::robot_interface
     constexpr aergo::module::communication_channel::Producer robot_interface_status_producer = {
         .channel_type_identifier_ = "robot_interface_status/v1:struct{feature:uint64}[+blob{data}]",
         .display_name_ = "Robot Status Message",
-        .display_description_ = "Asynchronous status messages from robot.",
-        .prioritized_ = false,
-        .message_queue_capacity_ = 16
+        .display_description_ = "Asynchronous status messages from robot."
+    };
+
+    constexpr aergo::module::communication_channel::Consumer robot_interface_status_consumer = {
+        .count_ = aergo::module::communication_channel::Consumer::Count::SINGLE,
+        .channel_type_identifier_ = robot_interface_status_producer.channel_type_identifier_,
+        .display_name_ = robot_interface_status_producer.display_name_,
+        .display_description_ = robot_interface_status_producer.display_description_,
+        .prioritized_ = true,
+        .message_queue_capacity_ = 4
     };
 
     constexpr aergo::module::communication_channel::Producer robot_interface_finished_producer = {
         .channel_type_identifier_ = "robot_interface_finished/v1:struct{action_id:uint64}[+blob{data}]",
         .display_name_ = "Robot Action Finished Message",
-        .display_description_ = "Asynchronous finished messages for long-running robot actions.",
+        .display_description_ = "Asynchronous finished messages for long-running robot actions."
+    };
+
+    constexpr aergo::module::communication_channel::Consumer robot_interface_finished_consumer = {
+        .count_ = aergo::module::communication_channel::Consumer::Count::SINGLE,
+        .channel_type_identifier_ = robot_interface_finished_producer.channel_type_identifier_,
+        .display_name_ = robot_interface_finished_producer.display_name_,
+        .display_description_ = robot_interface_finished_producer.display_description_,
         .prioritized_ = true,
         .message_queue_capacity_ = 16
     };
