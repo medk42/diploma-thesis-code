ecbbee7fb96a2e3b15c6200687ab7d45afff0e39
Jakub Medek <medekjak@gmail.com>
2025-12-13 14:34:34 +0100

    Rename helper to calibrated camera robot from stereo.

--- default_modules/robot_stereo_camera_calibration_module/src/robot_stereo_camera_calibration_module_contract.cpp
+++ default_modules/robot_stereo_camera_calibration_module/src/robot_stereo_camera_calibration_module_contract.cpp
@@ -7,7 +7,7 @@
 #include "module_helpers/activation_wrapper/activation_wrapper.h"
 #include "module_helpers/camera_pose_helper/message_structure.h"
 #include "module_helpers/parameter_description/parameter_description.h"
-#include "module_helpers/calibrated_stereo_robot_messages/calibrated_stereo_messages.h"
+#include "module_helpers/calibrated_camera_robot_messages/message_types.h"
 
 #include <limits>
 
@@ -20,7 +20,7 @@ using namespace aergo::module;
 using namespace aergo::default_modules::robot_stereo_camera_calibration_module;
 namespace cph = aergo::module::helpers::camera_pose_helper;
 namespace p_desc = aergo::module::helpers::parameter_description;
-namespace csr = aergo::module::helpers::calibrated_stereo_robot_messages;
+namespace ccrm = aergo::module::helpers::calibrated_camera_robot_messages;
 
 static constexpr communication_channel::Consumer subscribe_consumers[] = {
     {
@@ -38,7 +38,7 @@ static constexpr communication_channel::Producer response_producers[] = {
 };
 
 static constexpr communication_channel::Producer publish_producers[] = {
-    csr::calibrated_stereo_publish_producer
+    ccrm::calibrated_camera_publish_producer
 };
 
 static constexpr ModuleInfo module_info = {
