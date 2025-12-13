c000a938e58274bc91593c02a14f9ffec83472fd
Jakub Medek <medekjak@gmail.com>
2025-12-13 01:10:13 +0100

    Check failure of cv hand-eye calibration by seeing if the R/t matrices have expected values.

--- default_modules/robot_stereo_camera_calibration_module/src/calib/handeye_calibrator.cpp
+++ default_modules/robot_stereo_camera_calibration_module/src/calib/handeye_calibrator.cpp
@@ -55,20 +55,24 @@ namespace aergo::default_modules::robot_stereo_camera_calibration_module::calib
         }
 
         cv::Mat R_cam2gripper, t_cam2gripper; // output: camera -> gripper (flange<-cam, flange_from_cam, FC)
-        try
+
+        cv::calibrateHandEye(
+            R_gripper2base, t_gripper2base,
+            R_target2cam, t_target2cam,
+            R_cam2gripper, t_cam2gripper,
+            prm_.method
+        );
+
+        double eps = 1e-6;
+        const cv::Mat R_eye = cv::Mat::eye(3, 3, CV_64F);
+        const cv::Mat zero_t = cv::Mat::zeros(3, 1, CV_64F);
+
+        if (R_cam2gripper.size() != cv::Size(3, 3) ||
+            t_cam2gripper.size() != cv::Size(1, 3) ||
+            cv::norm(R_cam2gripper - R_eye) < eps &&
+            cv::norm(t_cam2gripper - zero_t) < eps)
         {
-            cv::calibrateHandEye(
-                R_gripper2base,
-                t_gripper2base,
-                R_target2cam,
-                t_target2cam,
-                R_cam2gripper,
-                t_cam2gripper,
-                prm_.method);
-        }
-        catch (const std::exception& e)
-        {
-            res.message = std::string("HandEyeCalibrator: exception during calibrateHandEye: ") + e.what();
+            res.message = "HandEyeCalibrator: unexpected output from calibrateHandEye.";
             return res;
         }
 
